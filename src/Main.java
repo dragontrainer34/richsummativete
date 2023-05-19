0f57be78282f4230d688726ca171a8cac88931a1
dragontrainer34 <carlos.kalambo.makungu@stu.ocsb.ca>
2023-05-19 10:57:39 -0400

    new commit

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,45 @@
 import java.util.*;
+
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		Deck d= new Deck();
+		Deck d = new Deck();
 		d.shuffle();
-		ArrayList<Card> P1=new ArrayList<>();
-		ArrayList<Card> P2=new ArrayList<>();
-		
-		for (int x=0;x<5;x++) {
+		ArrayList<Card> P1 = new ArrayList<>();
+		ArrayList<Card> P2 = new ArrayList<>();
+		ArrayList<Card> P3 = new ArrayList<>();
+		ArrayList<Card> P4 = new ArrayList<>();
+		ArrayList<Card> P5 = new ArrayList<>();
+
+		for (int x = 0; x < 5; x++) {
 			P1.add(d.deal());
 			P2.add(d.deal());
+			P3.add(d.deal());
+			P4.add(d.deal());
+			P5.add(d.deal());
 		}
 		System.out.println("Player 1");
-		for (Card c:P1) {
+		for (Card c : P1) {
 			System.out.println(c.getName());
 		}
-		System.out.println("Player 2");
-		for (Card c:P2) {
+		System.out.println("\nPlayer 2");
+		for (Card c : P2) {
 			System.out.println(c.getName());
 		}
-		System.out.println();
+		System.out.println("\nPlayer 3");
+		for (Card c : P3) {
+			System.out.println(c.getName());
+		}
+		System.out.println("\nPlayer 4");
+		for (Card c : P4) {
+			System.out.println(c.getName());
+		}
+
+		System.out.println("\nPlayer 5");
+		for (Card c : P5) {
+			System.out.println(c.getName());
+		}
 	}
 
 }
